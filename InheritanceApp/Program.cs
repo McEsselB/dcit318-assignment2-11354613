9e01eb27e21bb1d9a9bda1ecd8fc0b4d109f6cd0
McEsselB <mkbondzie@st.ug.edu.gh>
2025-08-04 14:54:47 +0000

    Completed Inheritance and Method Overriding app task

---
+++ InheritanceApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Animal
+{
+   public virtual void MakeSound()
+   {
+      Console.WriteLine("Some generic sound");
+   }
+}
+
+class Dog : Animal
+{
+   public override void MakeSound()
+   {
+      Console.WriteLine("Bark");
+   }
+}
+
+class Cat : Animal
+{
+   public override void MakeSound()
+   {
+      Console.WriteLine("Meow");
+   }
+}
+
+class Program
+{
+   static void Main()
+   {
+      Animal generic = new Animal();
+      Dog dog = new Dog();
+      Cat cat = new Cat();
+
+      generic.MakeSound();
+      dog.MakeSound();
+      cat.MakeSound();
+   }
+}
